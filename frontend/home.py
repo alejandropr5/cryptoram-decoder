9539e6e5feb3dff23db0d33afe8c6a7fabc103a3
alejandropr05 <alejandropr05@hotmail.com>
2023-10-19 22:13:43 -0500

    Refactor frontend/home.py

--- frontend/home.py
+++ frontend/home.py
@@ -1,5 +1,5 @@
 import streamlit as st
-import pandas as pd
+# import pandas as pd
 import requests
 import json
 import os
@@ -34,11 +34,8 @@ CIPHER_TEXT = (
 )
 
 
-def call_api_method(cipher_text: str, n_iter: int, n_pop: int) -> str:
-    request_data = {"cipher_text": cipher_text,
-                    "max_iter": n_iter,
-                    "n_population": n_pop}
-    request_data_json = json.dumps(request_data)
+def call_api_method(**kwargs) -> str:
+    request_data_json = json.dumps(kwargs)
     headers = {"Content-Type": "application/json"}
     decipher_method_endpoint = f"{API_ENDPOINT}/gencipher/decipher"
 
@@ -51,31 +48,29 @@ def call_api_method(cipher_text: str, n_iter: int, n_pop: int) -> str:
     return response_json["plain_text"], response_json["history"]
 
 
-def display_result(cipher_text: str, n_iter: int, n_pop: int) -> None:
-    with st.spinner("Processing..."):
-        response = call_api_method(cipher_text, n_iter, n_pop)
-        plain_text, history = response
-
+def display_result(plain_text: str) -> None:
+    # with st.spinner("Processing..."):
+    #     response = call_api_method(cipher_text, n_iter, n_pop)
+    #     plain_text, history = response
     st.header("Result")
     st.text_area(label="", value=plain_text, disabled=True)
 
-    with st.expander("Details"):
-        history_df = pd.DataFrame(history)
-        st.dataframe(
-            history_df,
-            use_container_width=True,
-            hide_index=False
-        )
+    # with st.expander("Details"):
+    #     history_df = pd.DataFrame(history)
+    #     st.dataframe(
+    #         history_df,
+    #         use_container_width=True,
+    #         hide_index=False
+    #     )
 
 
 def app():
     st.set_page_config(
         page_title="Cryptogram Decoder",
         page_icon="🔏",
-        layout="centered"
+        layout="wide"
     )
     st.title("Cryptogram Genetic Decoder")
-    st.markdown(HOME_TEXT)
 
     st.header("Input")
     cipher_text = st.text_area(
@@ -83,16 +78,34 @@ def app():
         value=CIPHER_TEXT
     )
 
-    with st.expander("Advanced options"):
-        col1, col2 = st.columns(2)
-        with col1:
-            n_iter = st.slider("Number of iterations", 0, 100, 25)
-        with col2:
-            n_pop = st.slider("Number of population", 0, 200, 120)
+    with st.sidebar:
+        st.title("Advanced options")
+        n_iter = st.slider("Number of iterations", 0, 100, 25)
+        n_pop = st.slider("Number of population", 0, 200, 120)
+        mutation_type = st.selectbox(
+            "Mutation algorithm",
+            ("insert", "swap", "inversion", "scramble")
+        )
+        mutation_rate = st.slider("Mutation rate", 0.0, 1.0, 0.01)
+        crossover_type = st.selectbox(
+            "Crosover algorithm",
+            ("full", "order-one", "partially-mapped", "cycle")
+        )
+        crossover_rate = st.slider("Crossover rate", 0.0, 1.0, 0.6)
 
     button_clicked = st.button("Decode")
     if button_clicked:
-        display_result(cipher_text, n_iter, n_pop)
+        with st.spinner("Processing..."):
+            plaint_text, history = call_api_method(
+                cipher_text=cipher_text,
+                max_iter=n_iter,
+                n_population=n_pop,
+                mutation_type=mutation_type,
+                mutation_rate=mutation_rate,
+                crossover_rate=crossover_rate,
+                crossover_type=crossover_type
+            )
+        display_result(plaint_text)
 
 
 if __name__ == "__main__":
