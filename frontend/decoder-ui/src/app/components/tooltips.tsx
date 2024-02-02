36bc2b6fb6d4d49e57e7d2805484318c21f41007
alejandropr05 <alejandropr05@hotmail.com>
2024-02-01 19:17:02 -0500

    Refactor tooltips

--- frontend/decoder-ui/src/app/components/tooltips.tsx
+++ frontend/decoder-ui/src/app/components/tooltips.tsx
@@ -22,11 +22,28 @@ export function LabelToolTip ({ children, text }: Props) {
   return (
     <div className="group relative w-fit">
       {children}
-      <div className="absolute left-[100%] top-0">
+      <div className="absolute left-[100%] top-0 w-fit">
         <div
-          className="invisible shadow-lg hidden bg-[#333] text-white font-semibold px-3 py-[6px] text-[13px] mx-auto w-max -top-3 rounded min-h-10 min-w-10
-          group-hover:visible group-hover:block absolute left-4 whitespace-normal max-w-[250px] h-auto z-50
+          className="invisible shadow-lg hidden bg-[#333] text-white font-light px-3 py-[6px] text-[13px] mx-auto -top-3 rounded min-w-10 text-wrap
+          group-hover:visible group-hover:block absolute left-4 whitespace-normal w-[180px] h-auto z-50
           before:w-4 before:h-4 before:rotate-45 before:bg-[#333] before:absolute before:top-3 before:-left-2 before:mx-auto"
+        >
+          {text}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export function ScoreToolTip ({ children, text }: Props) {
+  return (
+    <div className="group relative w-fit h-fit">
+      {children}
+      <div className="absolute -left-[53px] bottom-6">
+        <div
+          className="invisible shadow-lg hidden bg-[#333] text-white font-semibold px-3 py-[6px] text-[13px] mx-auto w-max rounded min-h-10 min-w-10 text-balance
+          group-hover:visible group-hover:block absolute whitespace-normal max-w-[250px] h-auto z-50 bottom-0
+          before:w-4 before:h-4 before:rotate-45 before:bg-[#333] before:absolute before:-bottom-2 before:left-[50%] before:mx-auto"
         >
           {text}
         </div>
