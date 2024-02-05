c8d4c6cc0a1929721c2316f0f97e150048380cad
alejandropr05 <alejandropr05@hotmail.com>
2024-02-05 14:22:37 -0500

    Make CustomHeader responsive

--- frontend/decoder-ui/src/app/components/header.tsx
+++ frontend/decoder-ui/src/app/components/header.tsx
@@ -9,17 +9,17 @@ import aboutSVG from '../../../public/web-svgrepo-com.svg'
 
 export function CustomHeader() {
     return (
-        <header className="mx-4 w-full h-[74px] bg-white border-b-[1px] border-b-[#eaecf0] sticky top-0 z-50">
-          <div className="mx-4 p-4 flex items-center justify-between">
+        <header className="w-full h-[74px] bg-white border-b-[1px] border-b-[#eaecf0] sticky top-0 z-50">
+          <div className="px-6 py-4 flex items-center justify-between">
             <div className="flex flex-row justify-between items-center space-x-2">
-              <div className="h-9 w-9">
+              <div className="max-h-9 max-w-9">
                 <ClientImage imageComponent={gencipher} description={'gencipher logo'}/>
               </div>
-              <span className="text-[#261c28] text-[28px] font-bold justify-between items-center tracking-widest">
+              <span className="text-[#261c28] text-lg min-[340px]:text-[28px] font-bold justify-between items-center tracking-widest">
                 Gencipher
               </span>
             </div>
-            <nav className="font-semibold mx-3 text-[12pt] flex flex-row space-x-4">
+            <nav className="font-semibold text-[12pt] flex flex-row space-x-4">
               <LinkRef linkText="GitHub" reference="https://github.com/alejandropr5/gencipher-page" imageComponent={githubSVG} description='GitHub logo'/>
               <LinkRef linkText="Author" reference="" imageComponent={aboutSVG} description='About logo'/>
             </nav>
