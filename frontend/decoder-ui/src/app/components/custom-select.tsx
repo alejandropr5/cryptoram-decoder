4acd7af0b9a36480916f00d9116a8b450c6bdbac
alejandropr05 <alejandropr05@hotmail.com>
2024-02-06 14:53:03 -0500

    Fix select input id

--- frontend/decoder-ui/src/app/components/custom-select.tsx
+++ frontend/decoder-ui/src/app/components/custom-select.tsx
@@ -45,7 +45,7 @@ export function CustomSelect(data: Props) {
         {...data.register(data.id, {
           required: true
         })}
-        id="countries"
+        id={data.id}
         className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 focus:outline-none mb-8"
         defaultValue={data.defaultVal}
       >
